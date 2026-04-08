7c31baaba14fb97e859afbe74f2a873f1d53c124
freeohariki1998 <freeohariki1998@gmail.com>
2026-04-09 04:07:58 +0900

    chromaとAIを使ってPDFを要約するAPIを作成

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import "dotenv/config"; //環境変数の読み込みショートカット
 import express from "express";
 import chatRouter from "./routes/chat";
 import summarizePdfRouter from "./routes/summarizePdf";
+import askRouter from "./routes/ask";
 
 const app = express();
 app.use(express.json());
 app.use("/api/chat", chatRouter);
 app.use("/api/summarize-pdf", summarizePdfRouter);
+app.use("/api/ask", askRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
